3eecdcdd4f56610a93f61619d9befc8b761a9f27
Ervin Varga <vargaervinerno0@gmail.comgit>
2022-10-19 14:31:57 +0200

    Iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-// Iteration 1: Names and Input
-//
+// Iteration 1: Names and Input//
+
+ let hacker1 = 'Ervin'
+ console.log("The driver's name is " + hacker1);
+
+ let hacker2 ='Anna'
+console.log("The navigator's name is " + hacker2);
+
 // Iteration 2: Conditionals
+
 
 
 // Iteration 3: Loops
